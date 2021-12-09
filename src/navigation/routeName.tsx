209bf2dfa8e726f4d6badf236b855a68ae583bc6
DESKTOP-0CGRU4P\DELL 3567 <vituonglaiconem@gmail.com>
2021-12-09 08:07:22 +0700

    add Login screen

--- src/navigation/routeName.tsx
+++ src/navigation/routeName.tsx
@@ -11,16 +11,3 @@ export const LOGIN = 'LOGIN';
 
 
 
-export const CHOOSE_ROLE = 'CHOOSE_ROLE';
-export const UPDATE_NEW_PASSWORD = 'UPDATE_NEW_PASSWORD';
-
-
-
-
-// AUTHENTICATION
-// export const TABBAR = 'TABBAR';
-// export const PROFILE = 'PROFILE';
-// export const BASIC_INFORMATION = 'HOME_PROFILE';
-// export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
-// export const PROFILE_LIFE_STYLE = 'PROFILE_LIFE_STYLE';
-
